59c96af5f17fdb30102594a48d81b059669866aa
PrathamShah29 <119159666+PrathamShah29@users.noreply.github.com>
2023-05-02 16:00:00 +0530

    Created sidebar component

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	images: {
+		domains: ["help.twitter.com", "img.freepik.com"],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
